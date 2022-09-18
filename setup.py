9bb7b5b2c00612bc244b330dc8d82475bc0281e8
u-brixton <un0blogger@gmail.com>
2022-09-18 17:06:56 +0300

    added requirements to setupfile, changed numpy deps

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
 from setuptools import setup, find_namespace_packages
 
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
 setup(name='pepe-semantics',
       version='0.0.2',
       package_dir={'': 'src'},
-      packages=find_namespace_packages(where='src')
+      packages=find_namespace_packages(where='src'),
+      install_requires=required,
      )
