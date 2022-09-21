ab2165f54b2dbdc5f91705d51d3f815ce4d6ea82
Job <jobquiroz@hotmail.com>
2022-09-21 02:43:53 -0500

    Removing gitignore and additional comments to download data

--- scripts/gif_scripts/download_data.py
+++ scripts/gif_scripts/download_data.py
@@ -1,12 +1,12 @@
 import gdown
-
+from pathlib import Path
 from pepe_semantics import config
 
 _GIDS_PATHS = (
     config.BERTWEET_WEIGHTS_PATHS,
     config.GIF_ID_TO_INFERRED_FEATURE_PATHS,
-    config.GIF_ID_TO_INFERRED_FEATURE_PATHS_FULL, # All features
     config.GIF_ID_TO_GIPHY_ID_PATHS,
+    #config.GIF_ID_TO_INFERRED_FEATURE_PATHS_FULL, # All features
 )
 
 def main():
