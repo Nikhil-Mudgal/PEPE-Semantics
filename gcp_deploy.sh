fe71d23549689bb4a632b8f82d473b26ccb0a7de
Job <jobquiroz@hotmail.com>
2022-09-21 03:53:07 -0500

    Bash script for deployment

--- gcp_deploy.sh
+++ gcp_deploy.sh
@@ -10,11 +10,14 @@ APP_NAME=$1
 IMAGE_NAME=$2
 TAG=$3
 
-PROJECT=$(gcloud config get-value project)
+PROJECT_ID=$(gcloud config get-value project)
 REPOSITORY_NAME=pepe-semantics
 
-# Submit a docker building tasks
-gcloud builds submit "." --region=us-central1 --tag us-central1-docker.pkg.dev/$PROJECT/$REPOSITORY_NAME/$IMAGE_NAME:$TAG
+# Submit a docker image-building task
+# This commands works properly directly on the CLI
+#gcloud builds submit . --region=us-central1 --tag us-central1-docker.pkg.dev/$PROJECT/$REPOSITORY_NAME/$IMAGE_NAME:$TAG
+# This commands works better when called from a bash script
+gcloud builds submit --config=cloudbuild.yaml --substitutions=REPO_NAME=$REPOSITORY_NAME,TAG_NAME=$TAG,REVISION_ID=$IMAGE_NAME
 
 # Deploy
-gcloud run deploy $APP_NAME --image us-central1-docker.pkg.dev/$PROJECT/$REPOSITORY_NAME/$IMAGE_NAME:$TAG --region=us-central1 --allow-unauthenticated
+gcloud run deploy $APP_NAME --image us-central1-docker.pkg.dev/$PROJECT_ID/$REPOSITORY_NAME/$IMAGE_NAME:$TAG --region=us-central1 --allow-unauthenticated
