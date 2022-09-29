1c4857baa93d91d5a3f355d77942d9b496c5a7e2
Job Quiroz <jobquiroz@hotmail.com>
2022-09-29 12:49:25 -0500

    Latest progress (#7)
    * Use paths from config and useless comments removed
    
    * Use Path instead of manual location. Full features Path added
    
    * Full features path added
    
    * utils removed. Not needed
    
    * .gitignore inside model directory removed
    
    * Added comment for full features file
    
    * Cosmetic changes
    
    * Added data folders to gitignore
    
    * Clean Dockerfile
    
    * dockerignore and gcloudignore added
    
    * Bash script for deploying to gcp added
    
    * Cleaner version
    
    * Adding data files to gitignore
    
    * Modifications for improved pip install flow
    
    * Removing gitignore and additional comments to download data
    
    * Hardcoding path
    
    * Modifications for local download
    
    * Using path from config.py
    
    * Clearner version
    
    * Bash script for deployment
    
    * Created github action
    
    * First test for continuous deployment with github actions
    
    * Added memory=4Gi for deployment in Cloud Run
    
    * More documentation
    
    * Documentation

--- gcp_deploy.sh
+++ gcp_deploy.sh
@@ -2,7 +2,7 @@
 
 if test "$#" -ne 3; then
    echo "Usage: ./gcp_deploy.sh app_name image_name tag"
-   echo "   eg: ./gcp_deploy.sh pepe_semantics pepe_sem_faster dev"
+   echo "   eg: ./gcp_deploy.sh pepe-semantics pepe-semantics-rev-1 dev"
    exit
 fi
 
@@ -16,8 +16,9 @@ REPOSITORY_NAME=pepe-semantics
 # Submit a docker image-building task
 # This commands works properly directly on the CLI
 #gcloud builds submit . --region=us-central1 --tag us-central1-docker.pkg.dev/$PROJECT/$REPOSITORY_NAME/$IMAGE_NAME:$TAG
+#gcloud builds submit --region=us-central1 --tag us-central1-docker.pkg.dev/$PROJECT/$REPOSITORY_NAME/$IMAGE_NAME:$TAG
 # This commands works better when called from a bash script
 gcloud builds submit --config=cloudbuild.yaml --substitutions=REPO_NAME=$REPOSITORY_NAME,TAG_NAME=$TAG,REVISION_ID=$IMAGE_NAME
 
 # Deploy
-gcloud run deploy $APP_NAME --image us-central1-docker.pkg.dev/$PROJECT_ID/$REPOSITORY_NAME/$IMAGE_NAME:$TAG --region=us-central1 --allow-unauthenticated
+gcloud run deploy $APP_NAME --image us-central1-docker.pkg.dev/$PROJECT_ID/$REPOSITORY_NAME/$IMAGE_NAME:$TAG --region=us-central1 --allow-unauthenticated --memory=4Gi
