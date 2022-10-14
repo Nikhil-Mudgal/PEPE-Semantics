47a709a5b86909b7f97d92c8244e06f3a2690d3e
u-brixton <un0blogger@gmail.com>
2022-10-14 17:04:05 +0400

    logging improvements

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from flask import Flask, request, jsonify
+import time
 import os
 from pathlib import Path
+
+from flask import Flask, request, jsonify, current_app, g as app_ctx
+
 from pepe_semantics import config
 from pepe_semantics.inference import InferenceModel
 from pepe_semantics.model.index import GIFIndex
@@ -17,6 +20,27 @@ def get_giphy_links(input_text, k = 5):
     """Get's giphy_id to the closest gif found"""
     text_emb = _MODEL.embed_text(input_text)
     return _INDEX.get_giphy_links(text_emb, k=k)
+
+@app.before_request
+def logging_before():
+    # Store the start time for the request
+    app_ctx.start_time = time.perf_counter()
+    current_app.logger.info(
+        f'Received request with input args "{request.json}" (method={request.method}, endpoint={request.path})'
+    )
+
+
+@app.after_request
+def logging_after(response):
+    # Get total time in milliseconds
+    total_time = time.perf_counter() - app_ctx.start_time
+    time_in_ms = int(total_time * 1000)
+    # Log the time taken for the endpoint
+    current_app.logger.info(
+        f'Request with input args "{request.json}" took {time_in_ms} ms, response is {response.json["result"]} (method={request.method}, endpoint={request.path})'
+    )
+    return response
+
 
 @app.route("/predict", methods=["POST"])
 def predict():
